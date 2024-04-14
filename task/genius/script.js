74ce677a67d98a21b4eeb93cb8bfc5e179004a36
Solractys <carloseduardosilvasouzadelima@gmail.com>
2024-04-14 21:03:47 +0000

    fix order

--- task/genius/script.js
+++ task/genius/script.js
@@ -4,63 +4,70 @@ let sequence = [];
 let playerSequence = [];
 let level = 1;
 
-// Essa função será reconstruida quando for disponibilizado os efeitos de audio para as teclas
-function playSound(number){
-  console.log(number);
+// Essa função será reconstruida quando for disponibilizado os efeitos sonoros para as teclas
+function PlaySound() {
+  console.log(sequence);
 }
-function AddToSequence () {
+function AddToSequence() {
   const randomButton = Math.floor(Math.random() * 9) + 1;
   sequence.push(randomButton);
-    setTimeout(() => {
-    playSequence();
-  }, 1000)
+  setTimeout(() => {
+    PlaySequence();
+    
+ }, 1000)
 }
-function playSequence() {
-  sequence.forEach((button, index) => {
+function PlaySequence() {
+  buttons.forEach( button => {
+    button.classList.remove('on'); 
+  })
+  sequence.forEach((index) => {
     setTimeout(() => {
-    playSound("numero");
-    }, 1000 * index);
+      buttons[index - 1].classList.add('on');
+      setTimeout(() => {
+        PlaySound();
+      }, 500);
+    }, 1000);
   });
 }
-function checkAnswer() {
-  for (let i = 0; i < playerSequence; i++){
-    if (playerSequence[i] !== sequence[i]){
-      gameOver();
+function CheckAnswer() {
+  for (let i = 0; i < playerSequence.length; i++) {
+    if (playerSequence[i] !== sequence[i]) {
+      GameOver();
       return;
     }
   }
-  if (playerSequence.length === sequence.length){
+  if (playerSequence.length === sequence.length) {
     level++;
     playerSequence = [];
     setTimeout(() => {
-    AddToSequence();
+      AddToSequence();
     }, 1000);
   }
+  if (level === 10) {
+    alert('You win! Yeah!');
+  }
 }
-function handleButtonClick(buttonNumber) {
+function HandleButtonClick(buttonNumber) {
   playerSequence.push(buttonNumber);
-  playSound(buttonNumber);
-  checkAnswer();
+  PlaySound(buttonNumber);
+  CheckAnswer();
 }
-function restartGame() {
+function RestartGame() {
   sequence = [];
   playerSequence = [];
   level = 1;
   AddToSequence();
 }
-function gameOver() {
+function GameOver() {
   alert('Game Over! Your score: ' + (level - 1));
-  restartGame()
+  console.log('Game Over!');
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove('on');
+  }
 }
-window.onload = restartGame();
 document.addEventListener('keydown', (event) => {
   const key = parseInt(event.key);
-  if (key >= 1 && key <= 9){
-    handleButtonClick(key);
+  if (key >= 1 && key <= 9) {
+    HandleButtonClick(key);
   }
-});
-
-
-
-
-
+  });
